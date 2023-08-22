cb762b2545a5e75e0737fc09ddd5e2c226d2701d
ghost <ghost@pop-os.localdomain>
2023-08-22 01:34:36 -0400

    Fixed horizontal win condition, strstr requires null termination

--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 #include <cstring>
 // STATE & GLOBALS.
     // x is true and o is false.
-    bool player = true;
-    bool playing = true;
-    int selection; 
+    static bool player = true;
+    static bool playing = true;
+    static int selection; 
 // Board (Multi-Dimensional Array).
     char board[3][3] = {
         {'#','#','#'},
@@ -29,7 +29,6 @@ bool validSelectionCheck(int selection) {
         std::cout << "Your selection isn't valid!" << '\n';
         return false;
     };
-    // Check if space is already taken.
     return true;
 };
 
@@ -37,30 +36,74 @@ void select(int selection) {
     validSelectionCheck(selection);
     switch(selection) {
         case 1: 
+            // Check if space is already taken.
+            if (board[2][0] == 'x' || board[2][0] == 'o') {
+                std::cout << "That space is already taken!" << '\n';
+                break;
+            };
             board[2][0] = player ? 'x' : 'o';
             break;
         case 2: 
+            // Check if space is already taken.
+            if (board[2][1] == 'x' || board[2][1] == 'o') {
+                std::cout << "That space is already taken!" << '\n';
+                break;
+            };
             board[2][1] = player ? 'x' : 'o';
             break;
         case 3:
+            // Check if space is already taken.
+            if (board[2][2] == 'x' || board[2][2] == 'o') {
+                std::cout << "That space is already taken!" << '\n';
+                break;
+            };
             board[2][2] = player ? 'x' : 'o';
             break;
         case 4:
+            // Check if space is already taken.
+            if (board[1][0] == 'x' || board[1][0] == 'o') {
+                std::cout << "That space is already taken!" << '\n';
+                break;
+            };
             board[1][0] = player ? 'x' : 'o';
             break;
         case 5: 
+            // Check if space is already taken.
+            if (board[1][1] == 'x' || board[1][1] == 'o') {
+                std::cout << "That space is already taken!" << '\n';
+                break;
+            };
             board[1][1] = player ? 'x' : 'o';
             break;
         case 6:
+            // Check if space is already taken.
+            if (board[1][2] == 'x' || board[1][2] == 'o') {
+                std::cout << "That space is already taken!" << '\n';
+                break;
+            };
             board[1][2] = player ? 'x' : 'o';
             break;
         case 7:
+            // Check if space is already taken.
+            if (board[0][0] == 'x' || board[0][0] == 'o') {
+                std::cout << "That space is already taken!" << '\n';
+                break;
+            };
             board[0][0] = player ? 'x' : 'o';
             break;
         case 8: 
+            // Check if space is already taken.
+            if (board[0][1] == 'x' || board[0][1] == 'o') {
+                break;
+            };
             board[0][1] = player ? 'x' : 'o';
             break;
         case 9:
+            // Check if space is already taken.
+            if (board[0][2] == 'x' || board[0][2] == 'o') {
+                std::cout << "That space is already taken!" << '\n';
+                break;
+            };
             board[0][2] = player ? 'x' : 'o';
             break;
     };   
@@ -71,44 +114,44 @@ void checkWin() {
     const char* x = "xxx";
     const char* o = "ooo";
     // Horizontal win.
-    char row1[3];
+    char row1[4];
     for (int i=0; i<3; i++) {
         for (int j=0; j<3; j++) {
             row1[j] = board[0][j];
         };
     };
-    char row2[3];
+    char row2[4];
     for (int i=0; i<3; i++) {
         for (int j=0; j<3; j++) {
             row2[j] = board[1][j];
         };
     };
-    char row3[3];
+    char row3[4];
     for (int i=0; i<3; i++) {
         for (int j=0; j<3; j++) {
-            row1[j] = board[2][j];
+            row3[j] = board[2][j];
         };
     };
-    // Pattern match check for each row
+    // Pattern match check for each row (strstr requires null termination)
     // -- X --
     char* xwin = strstr(row1, x);
     char* xwin1 = strstr(row2, x);
     char* xwin2 = strstr(row3, x);
     if (xwin || xwin1 || xwin2) {
-         boardState();
-         std::cout << "player X has won!" << '\n';
-         playing = false;
+        boardState();
+        std::cout << "player X has won!" << '\n';
+        playing = false;
     }
     // -- O --
     char* owin = strstr(row1, o);
     char* owin1 = strstr(row2, o);
     char* owin2 = strstr(row3, o);
     if (owin || owin1 || owin2) {
-         boardState();
-         std::cout << "player O has won!" << '\n';
-         playing = false;
+        boardState();
+        std::cout << "player O has won!" << '\n';
+        playing = false;
     }
-    // Vertical win
+    // Vertical win [0][1][1][2][2][3]
     // Diagonal win
     // Tie
 };
